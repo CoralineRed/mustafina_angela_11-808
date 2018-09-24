683f6145d6b917f9724f7ea4fffb45fa452eda2c
Angela <mustafinaanzhela@gmail.com>
2018-09-24 09:55:50 +0300

    [add] Cond1-2

---
+++ 2018/FALL/SEM/Cond1/Cond1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Cond1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            string begin = input[0];
+            string end = input[1];
+            if (Math.Abs(begin[0] - end[0]) == Math.Abs(begin[1] - end[1])
+                && (begin[0] - end[0] + begin[1] - end[1] != 0))
+                Console.WriteLine("Correct move for bishop");
+            else Console.WriteLine("Incorrect move for bishop");
+
+            if ((Math.Abs(begin[0] - end[0]) == 2 && Math.Abs(begin[1] - end[1]) == 1
+                || Math.Abs(begin[0] - end[0]) == 1 && Math.Abs(begin[1] - end[1]) == 2)
+                && (begin[0] - end[0] + begin[1] - end[1] != 0))
+                Console.WriteLine("Correct move for knight");
+            else Console.WriteLine("Incorrect move for knight");
+
+            if ((Math.Abs(begin[0] - end[0]) + Math.Abs(begin[1] - end[1]) == Math.Abs(begin[0] - end[0])
+                || Math.Abs(begin[0] - end[0]) + Math.Abs(begin[1] - end[1]) == Math.Abs(begin[1] - end[1]))
+                && (begin[0] - end[0] + begin[1] - end[1] != 0))
+                Console.WriteLine("Correct move for rook");
+            else Console.WriteLine("Incorrect move for rook");
+
+            if ((Math.Abs(end[0] - begin[0]) == Math.Abs(end[1] - begin[1]) || Math.Abs(end[1] - begin[1]) == 0 || Math.Abs(end[0] - begin[0]) == 0)
+                && (Math.Abs(end[0] - begin[0]) + Math.Abs(end[1] - begin[1]) != 0))
+                Console.WriteLine("Correct move for queen");
+            else Console.WriteLine("Incorrect move for queen");
+
+            if (Math.Abs(begin[0] - end[0]) == Math.Abs(begin[1] - end[1]) && Math.Abs(begin[0] - end[0]) == 1)
+                Console.WriteLine("Correct move for king");
+            else Console.WriteLine("Incorrect move for king");
+        }
+    }
+}
---
+++ 2018/FALL/SEM/Cond2/Cond2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cond2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split();
+            string[] input2 = Console.ReadLine().Split();
+            int x = int.Parse(input1[0]);
+            int y = int.Parse(input1[1]);
+            int z = int.Parse(input1[1]);
+            int a = int.Parse(input2[0]);
+            int b = int.Parse(input2[1]);
+            if (x > y)
+            {
+                int p = x;
+                x = y;
+                y = p;
+            }
+            if (x > z)
+            {
+                int p = x;
+                x = z;
+                z = p;
+            }
+            if (x <= Math.Min(a, b) && Math.Min(y, z) <= Math.Max(a, b))
+                Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
