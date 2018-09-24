ba26b8df80fc4220ac45556f8adbc7c2d308cf4c
Angela <mustafinaanzhela@gmail.com>
2018-09-24 08:45:15 +0300

    [add] Distance, Pluralize, Rectangles

---
+++ 2018/FALL/PR/Distance/DistanceTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DistanceTask
+{
+	public static class DistanceTask
+	{
+		// Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
+		public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
+		{
+            if (ax == bx)
+                if (y <= Math.Max(ay, by) && y >= Math.Min(ay, by))
+                    return Math.Abs(ax - x);
+            if (ay == by)
+                if (x <= Math.Max(ax, bx) && x >= Math.Min(ax, bx))
+                    return Math.Abs(ay - y);
+            // Находим прямую y = k*x + b, проходящую через отрезок.
+            double k = (double)(ay - by) / (ax - bx);
+            double b = (double)(ax * by - bx * ay) / (ax - bx);
+            // Находим прямую y = -x/k + c, проходящую через точку и перпендикулярную отрезку.
+            double c = y + x / k;
+            double x1 = (c - b) / (k + 1 / k);
+            double y1 = k * x1 + b;
+            if (x1 <= Math.Max(ax, bx) && x1 >= Math.Min(ax, bx))
+                return Math.Sqrt((x1 - x) * (x1 - x) + (y1 - y) * (y1 - y));
+            else
+                return Math.Sqrt(Math.Min((ax - x) * (ax - x) + (ay - y) * (ay - y),
+                    (bx - x) * (bx - x) + (by - y) * (by - y)));
+		}
+	}
+}
---
+++ 2018/FALL/PR/Pluralize/PluralizeTask.cs
@@ -0,0 +1,12 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+            if (count % 10 == 1 && count % 100 != 11) return "рубль";
+            else if (count % 10 > 1 && count % 10 < 5 && !(count % 100 > 10 && count % 100 < 20)) return "рубля";
+			else return "рублей";
+		}
+	}
+}
